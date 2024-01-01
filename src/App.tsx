bc555f5d15f83aec6a667d040c3ef6147f86f95a
AlexZhdanov <zhora0@mail.ru>
2024-01-01 23:12:20 +0300

    0-10 Not ended

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
+      <div className='container'>
+          <Header/>
+          <div className='container_main'>
+              <Sidebar/>
+              <Content/>
+          </div>
+      </div>
+
   );
+}
+
+const Header = () => {
+    return (
+        <div className="Header">
+            Hello, samurai! Let's go!
+        </div>
+    )
+}
+const Sidebar = () => {
+    return (
+        <div className="Sidebar">
+            SIDESIDEBAR
+        </div>
+    )
+}
+const Content = () => {
+    return (
+        <div className="Content">
+            MAINMAIN
+        </div>
+    )
 }
 
 export default App;
