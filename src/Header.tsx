8c2053bdf2255be54b9ed80a44f69357d7e91ace
AlexZhdanov <zhora0@mail.ru>
2024-01-03 22:05:27 +0300

    14 profile fixed

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,6 +4,9 @@ import s from './Header.module.css'
 export const Header = () => {
     return (
         <div className={s.header}>
+            <img className={s.header__logo}
+                src="https://cdn-icons-png.flaticon.com/512/226/226772.png"
+                alt=""/>
             Hello, samurai! Let's go!
         </div>
     )
--- src/Profile.tsx
+++ src/Profile.tsx
@@ -4,20 +4,35 @@ import s from './Profile.module.css'
 export const Profile = () => {
     return (
         <div className={s.profile}>
-            <div>
-                <img
-                    src="https://bogatyr.club/uploads/posts/2021-11/thumbs/1636964350_6-bogatyr-club-p-krasivii-fon-goroda-6.jpg"
-                    alt=""/>
-            </div>
-            <div>ava + discription</div>
-            <div>
-                My posts
+
+            <img className={s.profile__img}
+                 src="https://interier-foto.ru/wp-content/uploads/dlinnye-foto-4.jpg"
+                 alt=""/>
+            <div className={s.profile__wrapper}>
+                <div className={s.profile__block}>
+                    <img className={s.avatar}
+                         src="https://cdn-icons-png.flaticon.com/512/3607/3607444.png" alt=""/>
+                    <div className={s.discriprion}>
+                        <span>Alex Жданович</span>
+                        <span>Date of birthday: 20 july 2000</span>
+                        <span>City:Minsk</span>
+                        <span>Education: BNTU</span>
+                        <span>Web-site:none</span>
+                    </div>
+                </div>
                 <div>
-                    NEW POST
-                    <div className={s.item}>post1</div>
-                    <div className={s.item}>post2</div>
+                    <h3>My posts</h3>
+                    <div>
+                        <input className={s.posts__input} type="text" placeholder="Введите сообщение..."/>
+                        <button className={s.posts__button}>send</button>
+                        <div className={s.posts__block}>
+                            <div className={s.item}>post1</div>
+                            <div className={s.item}>post2</div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
         </div>
     )
 }
