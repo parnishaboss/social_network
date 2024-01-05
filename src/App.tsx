6567999053e7a408733f2ad4c38ca2dd7cd2fdc6
AlexZhdanov <zhora0@mail.ru>
2024-01-05 15:39:43 +0300

    19 route

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,25 @@ import {Header} from './Header/Header';
 import {Navbar} from './Navbar/Navbar';
 import {Profile} from './Profile/Profile';
 import {Dialogs} from './Dialogs/Dialogs';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
-  return (
-      <div className='container'>
-          <Header/>
-          <div className='app__wrapper'>
-              <Navbar/>
-              <div className='app__wrapper__content'>
-                  {/*<Profile/>*/}
-                  <Dialogs/>
-              </div>
-          </div>
-      </div>
+    return (
+        <BrowserRouter>
+            <div className="container">
+                <Header/>
+                <div className="app__wrapper">
+                    <Navbar/>
+                    <div className="app__wrapper__content">
+                        <Route path='/profile' component={Profile}/>
+                        <Route path='/dialogs' component={Dialogs}/>
+                    </div>
+                </div>
+            </div>
+        </BrowserRouter>
 
-  );
+    );
 }
-
-
-
 
 
 export default App;
--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -6,22 +6,27 @@ export const Navbar = () => {
     return (
         <div className={s.navbar}>
             <div className={s.item__block}>
-                <a className={s.item} href="">
+                <a className={s.item} href="/profile">
                     Profile
                 </a>
             </div>
             <div className={s.item__block}>
-                <a className={s.item} href="">
+                <a className={s.item} href="/dialogs">
+                    Dialogs
+                </a>
+            </div>
+            <div className={s.item__block}>
+                <a className={s.item} href="/messages">
                     Messages
                 </a>
             </div>
             <div className={s.item__block}>
-                <a className={`${s.item} ${s.active}`} href="">
+                <a className={`${s.item} ${s.active}`} href="/news">
                     News
                 </a>
             </div>
             <div className={s.item__block}>
-                <a className={s.item} href="">
+                <a className={s.item} href="/settings">
                     Settings
                 </a>
             </div>
