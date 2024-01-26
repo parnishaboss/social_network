e0571af7f582e87e82378ffd303406c9deedc747
AlexZhdanov <zhora0@mail.ru>
2024-01-26 11:49:36 +0300

    24

--- src/Dialogs/Dialogs.tsx
+++ src/Dialogs/Dialogs.tsx
@@ -4,20 +4,36 @@ import {DialogsItem} from './DialogItem/DialogsItem';
 import {Message} from './Message/Message';
 
 export const Dialogs = () => {
+
+    let dialogsData = [
+        {id: 1, name: 'Sasha'},
+        {id: 2, name: 'Sveta'},
+        {id: 3, name: 'Misha'},
+        {id: 4, name: 'Vasya'},
+        {id: 5, name: 'Kolya'},
+    ]
+    let messagesData = [
+        {message: 'Privet'},
+        {message: 'How are you '},
+        {message: 'I am from London !'}
+    ]
     return (
         <div className={s.dialogs}>
             <div className={s.dialogs__item}>
-                <DialogsItem name='Sasha' id={1}/>
-                <DialogsItem name='Sveta' id={2}/>
-                <DialogsItem name='Misha' id={3}/>
-                <DialogsItem name='Vasya' id={4}/>
-                <DialogsItem name='Kolya' id={5}/>
+                {/*{dialogsData.map((d) => {*/}
+                {/*    <DialogsItem name={d.name} id={d.id}/>*/}
+                {/*})}*/}
+                <DialogsItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogsItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogsItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <DialogsItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <DialogsItem name={dialogsData[4].name} id={dialogsData[4].id}/>
             </div>
             <div>
                 <div className={s.messages}>
-                    <Message message='Privet'/>
-                    <Message message='How are you ?'/>
-                    <Message message='I am from London !'/>
+                    <Message message={messagesData[0].message}/>
+                    <Message message={messagesData[1].message}/>
+                    <Message message={messagesData[2].message}/>
                 </div>
             </div>
         </div>
--- src/Profile/MyPosts/MyPosts.tsx
+++ src/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,11 @@ import s from './MyPosts.module.css'
 import {Post} from './Post/Post';
 
 export const MyPosts = () => {
+    let postsData = [
+        {id:1, message: 'first post', likesCount: 21},
+        {id:2,message: 'second post', likesCount: 42},
+        {id:3,message: 'third post', likesCount: 12},
+    ]
     return (
         <div>
             <h3>My posts</h3>
@@ -10,9 +15,9 @@ export const MyPosts = () => {
                 <input className={s.posts__input} type="text" placeholder="Введите сообщение..."/>
                 <button className={s.posts__button}>send</button>
                 <div className={s.posts__block}>
-                    <Post message='first post' likesCount={23}/>
-                    <Post message='second post' likesCount={123}/>
-                    <Post message='third post' likesCount={3}/>
+                    <Post message={postsData[0].message} likesCount={postsData[0].likesCount}/>
+                    <Post message={postsData[1].message} likesCount={postsData[1].likesCount}/>
+                    <Post message={postsData[2].message} likesCount={postsData[2].likesCount}/>
                 </div>
             </div>
         </div>
